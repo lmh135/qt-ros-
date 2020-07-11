f3b9ad48db6857041c7ff5940a37e6cc26fcc3f2
lmh <aminghli@163.com>
2020-07-11 18:51:15 +0800

    qt run ros

---
+++ OccupanyMappingProject/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/liminghui/lmh-scut/qt运行调试ros工程/作业/OccupanyMappingProject/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/liminghui/lmh-scut/qt运行调试ros工程/作业/OccupanyMappingProject/devel;/opt/ros/KINETIC;/opt/ros/kinetic".split(';') if "/home/liminghui/lmh-scut/qt运行调试ros工程/作业/OccupanyMappingProject/devel;/opt/ros/KINETIC;/opt/ros/kinetic" != "" else []
---
+++ OccupanyMappingProject/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD="/home/liminghui/lmh-scut/qt运行调试ros工程/作业/OccupanyMappingProject/build"
+export ROS_PACKAGE_PATH="/home/liminghui/OccupanyMappingProject/src:/home/liminghui/lmh-scut/qt运行调试ros工程/作业/OccupanyMappingProject/src:/opt/ros/KINETIC/share:/opt/ros/kinetic/share"
